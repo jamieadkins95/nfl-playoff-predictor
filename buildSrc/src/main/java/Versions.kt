d07958fd0fbc37ab048d9360f7a724ae27920073
Jamie Adkins <jamieadkins95@gmail.com>
2019-12-21 15:08:32 +0000

    Initial project setup

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,48 @@
+object Versions {
+    const val compile_sdk = 28
+    const val min_sdk = 21
+    const val target_sdk = 28
+
+    const val gradle_plugin = "3.5.3"
+    const val kotlin = "1.3.50"
+
+    const val app_version_name = "1.0.0"
+    const val app_version_code = 1
+
+    const val androidx_core = "1.1.0"
+    const val androidx_appcompat = "1.1.0"
+    const val androidx_constraint_layout = "2.0.0-beta3"
+    const val material_design = "1.1.0-rc01"
+    const val nav_component = "2.2.0-rc04"
+
+    const val retrofit = "2.4.0"
+    const val okhttp = "3.8.0"
+    const val okio = "1.13.0"
+    const val rxjava = "2.1.12"
+    const val rxandroid = "2.0.2"
+    const val rxpreferences = "2.0.0"
+    const val gson = "2.8.2"
+    const val curl = "0.3.1"
+    const val dagger = "2.24"
+    const val glide = "4.9.0"
+
+    const val joda = "2.9.9"
+    const val groupie = "2.3.0"
+
+    // Logging
+    const val crashlytics = "2.9.1@aar"
+    const val fabric = "1.31.2"
+    const val timber = "4.7.0"
+
+    // TEST
+    const val junit = "4.12"
+    const val mockito = "2.22.0"
+    const val mockito_kotlin = "1.5.0"
+    const val espresso = "3.1.0"
+    const val androidx_test_core = "1.0.0"
+    const val androidx_test_runner = "1.1.0"
+    const val androidx_test_rules = "1.1.0"
+    const val androidx_junit = "1.0.0"
+    const val androidx_truth = "1.1.0"
+    const val fragment_test = "1.1.0-alpha05"
+}
